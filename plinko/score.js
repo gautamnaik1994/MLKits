adf44d081e2363ac80d98925dd00fb0685bd9594
Gautam Naik <gautamnaik1994@gmail.com>
2019-02-21 18:56:09 +0530

    Add files via upload

---
+++ plinko/score.js
@@ -0,0 +1,40 @@
+const outputs = [];
+const k = 3;
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+  const [testSet, trainingSet] = splitDataset(outputs, 10);
+
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+    console.log(bucket, testSet[i][3]);
+  }
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(rows => rows[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+}
+
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+  return [testSet, trainingSet];
+}
